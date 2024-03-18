f11531df084d003d5ca7ceda94a67c7513f4fbfd
Sherbakov Mikhail <michailshcherbakov@gmail.com>
2024-03-18 10:12:40 +0300

    fix(build): update build config

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,10 @@ import * as esbuild from "esbuild";
 
 await esbuild.build({
   outdir: "dist",
-  entryPoints: ["common", "srp", "auk"].map((module) => ({ in: `./src/${module}/index.ts`, out: module })),
+  entryPoints: ["common", "key_sets", "srp", "vault_items", "vaults"].map((module) => ({
+    in: `./src/${module}/index.ts`,
+    out: module,
+  })),
   platform: "neutral",
   target: "es2020",
   bundle: true,
